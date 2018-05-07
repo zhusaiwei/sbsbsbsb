0c394acc40c1d319dd23ccee98b0905a6c633b14
zhusaiwei <17319303770@163.com>
2018-05-07 19:11:52 +0800

    first commit

---
+++ script/token.js
@@ -0,0 +1,37 @@
+  var token = $api.getStorage('token');
+  var ajpush_userId = $api.getStorage('ajpush_userId');
+  //alert(token);
+
+    //var token = $.cookie("token");
+    //var token=localStorage.getItem("token");
+//alert(token);
+        //if(token==null||!token){
+          //  window.location.href="binding.html";
+//alert("token 不存在");
+        //}
+        // window.location.href = "login.html";
+        // //如果token不存在则重新赋予token存在cookie中
+        // $.ajax({
+        //     type : "get",
+        //     url : ip + "/admin/weChatIndex",
+        //     success : function () {
+        //        // if(data.code==2){
+        //        //     $.cookie("token",data.token);
+        //        //     location.href="index1.html";
+        //        // }
+        //
+        //     },
+        //     error : function () {
+        //         alert("AJAX ERROR");
+        //         return;
+        //     }
+        // });
+
+   // }
+    // console.log(token);
+    // alert(456);
+    // if(token===null||!token){
+    //     alert(123);
+    //     window.location.href = "login.html";
+    // }
+    // (token===null||!token)?window.location.href = "login.html":"";
